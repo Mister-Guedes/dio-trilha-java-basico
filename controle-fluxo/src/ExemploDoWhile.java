e6f43d78e15ca5cd0572c6908dce48f09267cfe7
Arthur Guedes <arthurhenrique96120@gmail.com>
2024-07-16 18:21:35 -0300

    .

--- controle-fluxo/src/ExemploDoWhile.java
+++ controle-fluxo/src/ExemploDoWhile.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 		do {
 			//excutando repetidas vezes até alguém atender
 			System.out.println("Telefone tocando");
-
+		
 		}while(tocando());
 
 		System.out.println("Alô !!!");
